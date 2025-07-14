ebd86de6168ddbd8a7366c991cc53ad80a3f4516
akhilkumar4464 <niteshsharma4464@gmail.com>
2025-07-14 19:31:34 +0530

    mern basics part 1

---
+++ node.js/TODO/todo.js
@@ -0,0 +1,55 @@
+ const fs = require('fs');
+ const Filepath = './tasks.json';
+
+ const loadTasks = () => {
+     try {
+          const dataBuffer =   fs.readFileSync(Filepath); // Read the file synchronously
+            const dataJSON = dataBuffer.toString(); // Convert buffer to string
+ return JSON.parse(dataJSON)
+     
+     } catch (error) {
+         return [];
+     }
+    
+    }
+
+    const saveTasks = (tasks) => {
+          const dataJSON= JSON.stringify(tasks); // Convert tasks to JSON string
+              fs.writeFileSync(Filepath, dataJSON); // Write JSON string to file
+        
+    }
+ const addtask = (task)=>{
+      const tasks = loadTasks();
+      tasks.push({ task });
+      saveTasks(tasks);
+        console.log(`Task "${task}" added successfully.`);
+       
+       
+
+     
+ }
+
+ const listtask =()=>{
+    const tasks = loadTasks()
+
+    tasks.map((task , index) => {
+   console.log(`${index + 1}. ${task.task}`);
+    })
+ }
+
+ const command = process.argv[2];
+ const argument = process.argv[3];
+
+
+
+ if ( command === 'addtask'){
+    addtask(argument)
+ }else if ( command === 'listtask'){
+    listtask()
+    }else if ( command === 'removetask'){
+    removetask(parseInt(argument))
+    }else {
+    console.log('Invalid command. Please use addtask, listtask, or removetask.');
+    }
+
+
